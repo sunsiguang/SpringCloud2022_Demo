276479842900375e3c1cfefc31a1b06dee6eef4c
sunsiguang <sunsiguang@cnrmall.com>
2022-12-21 15:32:07 +0800

    提交启动 .idea/runConfigurations

--- cloud-payment8001/src/main/java/com/cnrmall/springcloud/controller/PaymentController.java
+++ cloud-payment8001/src/main/java/com/cnrmall/springcloud/controller/PaymentController.java
@@ -10,14 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
